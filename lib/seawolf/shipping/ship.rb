e235b3e0ef753d23bae30be606bede4f55a3222a
Fredrik Persen Fostvedt <fpfostvedt@gmail.com>
2017-09-04 22:21:53 +1000

    hide unused method

--- lib/seawolf/shipping/ship.rb
+++ lib/seawolf/shipping/ship.rb
@@ -13,15 +13,17 @@ class Ship
     amount - shippable_amount(amount)
   end
 
-  def shippable_amount(amount)
-    capacity * number_of_possible_trips(amount)
-  end
-
   def can_ship_amount?(amount)
     capacity <= amount
   end
 
   def can_ship_entire_amount?(amount)
     unshippable_amount(amount) == 0
+  end
+
+private
+
+  def shippable_amount(amount)
+    capacity * number_of_possible_trips(amount)
   end
 end
