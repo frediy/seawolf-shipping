790bc7eb112364f9b6b24e908ce592c5eae864a9
Fredrik Persen Fostvedt <fpfostvedt@gmail.com>
2017-09-05 11:49:42 +1000

    fix calculator specs. minor refactoring of calculator

--- lib/seawolf/shipping/calculator.rb
+++ lib/seawolf/shipping/calculator.rb
@@ -3,28 +3,37 @@ class Calculator
     @ships, @amount_to_transport = ships, amount_to_transport
   end
 
-  def calculate()
-    amount_to_smallest_schedule = {}
+  def minimum_ships
+    amount_to_minimum_ships = {}
 
     ships_sorted_by_lowest_to_highest_capacity(@ships).each do |ship|
       (1..@amount_to_transport).each do |amount|
 
         if ship.can_ship_amount?(amount)
-          amount_to_smallest_schedule[amount] = [ship] * ship.number_of_possible_trips(amount)
+          new_ships = [ship] * ship.number_of_possible_trips(amount)
 
           unless ship.can_ship_entire_amount?(amount)
-            amount_to_smallest_schedule[amount] += amount_to_smallest_schedule[ship.unshippable_amount(amount)]
+            new_ships += amount_to_minimum_ships[ship.unshippable_amount(amount)]
+          end
+
+          if new_ships_decreases_minimum_ships?(amount_to_minimum_ships, new_ships, amount)
+            amount_to_minimum_ships[amount] = new_ships
           end
         end
       end
     end
 
-    amount_to_smallest_schedule[@amount_to_transport]
+    amount_to_minimum_ships[@amount_to_transport]
   end
 
 private
 
   def ships_sorted_by_lowest_to_highest_capacity(ships)
     ships.sort_by { |ship| ship.capacity }
+  end
+
+  def new_ships_decreases_minimum_ships?(amount_to_minimum_ships, new_ships, amount)
+    amount_to_minimum_ships[amount].nil? ||
+    new_ships.count <= amount_to_minimum_ships[amount].count
   end
 end
--- spec/lib/seawolf/shipping/calculator_spec.rb
+++ spec/lib/seawolf/shipping/calculator_spec.rb
@@ -7,37 +7,49 @@ RSpec.describe Calculator do
 
   let(:ships) do
     [
-      Ship.new(identifier: 'S', name: 'Speedy',  capacity: 1),
-      Ship.new(identifier: 'E', name: 'El Gamo', capacity: 6),
-      Ship.new(identifier: 'P', name: 'Preuben', capacity: 8)
+      Ship.new(identifier: 'A', name: 'Alpha',  capacity: 1),
+      Ship.new(identifier: 'B', name: 'Bravo', capacity: 6),
+      Ship.new(identifier: 'C', name: 'Charlie', capacity: 8)
     ]
   end
 
-  describe '#calculate' do
-    subject(:calculate) { calculator.calculate }
+  describe '#minimum_ships' do
+    subject(:minimum_ships) { calculator.minimum_ships }
+
+    let(:ship_a_count) { minimum_ships.select { |ship| ship.identifier == 'A' }.count }
+    let(:ship_b_count) { minimum_ships.select { |ship| ship.identifier == 'B' }.count }
+    let(:ship_c_count) { minimum_ships.select { |ship| ship.identifier == 'C' }.count }
 
     context 'returns single ship with one trip' do
       let(:amount_to_transport) { 8 }
 
-      specify { p calculate }
+      specify { expect(ship_a_count).to eq 0 }
+      specify { expect(ship_b_count).to eq 0 }
+      specify { expect(ship_c_count).to eq 1 }
     end
 
     context 'returns multiple ships with one trip' do
       let(:amount_to_transport) { 14 }
 
-      specify { p calculate }
+      specify { expect(ship_a_count).to eq 0 }
+      specify { expect(ship_b_count).to eq 1 }
+      specify { expect(ship_c_count).to eq 1 }
     end
 
     context 'returns single ship with multiple trips' do
       let(:amount_to_transport) { 12 }
 
-      specify { p calculate }
+      specify { expect(ship_a_count).to eq 0 }
+      specify { expect(ship_b_count).to eq 2 }
+      specify { expect(ship_c_count).to eq 0 }
     end
 
     context 'returns multiple ships with multiple trips' do
       let(:amount_to_transport) { 20 }
 
-      specify { p calculate }
+      specify { expect(ship_a_count).to eq 2 }
+      specify { expect(ship_b_count).to eq 3 }
+      specify { expect(ship_c_count).to eq 0 }
     end
   end
 end
